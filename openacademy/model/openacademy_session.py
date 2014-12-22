6e3aaf6b4996e6c3b8f9c7f71a04a6f90926389a
Luis Miguel Varon <sistemas@colorisa.com.co>
2014-12-22 14:10:21 -0500

    [REF] openacademy: Adding mm field

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import fields, models
+from openerp import fields, models, api
 
 class Session(models.Model):
 	_name = 'openacademy.session'
@@ -10,5 +10,6 @@ class Session(models.Model):
 	duration = fields.Float(digits=(6, 2), help="Duration in days")
 	seats = fields.Integer(string="Number of seats")
 	instructor_id = fields.Many2one('res.partner', string="Instructor")
-	course_id = fields.Many2many('openacademy.course',
+	course_id = fields.Many2one('openacademy.course',
 								ondelete='cascade', string="Course", required=True)
+	attendee_ids = fields.Many2many('res.partner', string="Attendees")
