b940b3dff77c54cc8118cd0af204506e301b4f3c
Luis Miguel Varon <sistemas@colorisa.com.co>
2014-12-29 13:07:12 -0500

    [REF] openacademy: Adding python constrains

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import fields, models, api
+from openerp import fields, models, api, exceptions
 
 class Session(models.Model):
 	_name = 'openacademy.session'
@@ -26,4 +26,26 @@ class Session(models.Model):
 		if not self.seats:
 			self.taken_seats = 0
 		else:
-			self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats	
+			self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
+		
+	@api.onchange('seats', 'attendee_ids')
+	def _verify_valid_seats(self):
+		if self.seats < 0:
+			return {
+					'warning': {
+					'title': "Incorrect 'seats' value",
+					'message': "The number of available seats may not be negative",
+					},
+			}
+		if self.seats < len(self.attendee_ids):
+			return {
+					'warning': {
+					'title': "Too many attendees",
+					'message': "Increase seats or remove excess attendees",
+					},
+				}
+	@api.one
+	@api.constrains('instructor_id', 'attendee_ids')
+	def _check_instructor_not_in_attendees(self):
+		if self.instructor_id and self.instructor_id in self.attendee_ids:
+			raise exceptions.ValidationError("A session's instructor can't be an attendee")
